2009fbf268635c2f09ca2147876c9f08cacb6e5a
emmavdheide <erv5124@psu.edu>
2026-03-20 16:31:48 -0400

    Updated code to exclude treatments that experienced both early and late mow

--- Pre Mowing LLL Analysis.R
+++ Pre Mowing LLL Analysis.R
@@ -20,50 +20,20 @@ LLL_combined$Year<-as.factor(LLL_combined$Year)
 LLL_filtered <- LLL_combined[!(LLL_combined$Treatment == "Rosette Damage/Early Mow/Late Mow"|LLL_combined$Treatment=="Early Mow/Late Mow"), ]
 
 #run an ANOVA
-anova_combined<-aov(lm(PreMowLLL~Treatment, data=LLL_combined))
-
 anova_combined_filt<-aov(lm(PreMowLLL~Treatment, data=LLL_filtered))
 
 #check residuals
-plot(lm(PreMowLLL~Treatment, data=LLL_combined)) #looks fine
-
-plot(lm(PreMowLLL~Treatment, data=LLL_filtered))
+plot(lm(PreMowLLL~Treatment, data=LLL_filtered)) #looks fine
 
 #summarize anova
-summary(anova_combined)
-#at least one group is significantly different
-
 summary(anova_combined_filt)
 #at least one group is significantly different
 
 #Post-hoc comparisons
-emmeans(anova_combined, specs = pairwise~Treatment)
-cld(emmeans(anova_combined, specs = pairwise~Treatment), Letters=letters)
-
-#RD+LM significantly shorter than LM
-#RD+EM significantly shorter than EM
-#RD+EM+LM significantly shorter than EM+LM
-
 emmeans(anova_combined_filt, specs = pairwise~Treatment)
 cld(emmeans(anova_combined_filt, specs = pairwise~Treatment), Letters=letters)
 #RD+LM significantly shorter than LM
 #RD+EM significantly shorter than EM
-
-#Alternatively, t test rosette damage vs. no rosette damage
-#separate rosette damaged and non-rosette damaged plants
-#DATA IS SET UP DIFFERENTLY, SO THIS DOESN'T WORK YET
-LLL_RD<-LLL_combined[LLL_combined$RD=="Y",]
-LLL_NoRD<-LLL_combined[LLL_combined$RD=="N",]
-
-t.test(LLL_RD$PreMowLLL, LLL_NoRD$PreMowLLL)
-#Again, this shows that rosette damaged plants have shorter LLL before mowing
-
-#And t-test RD+LM vs. RD+EM for effect of timing
-LLL_RDLM<-LLL_combined[LLL_combined$Treatment=="Rosette Damage/Late Mow",]
-LLL_RDEM<-LLL_combined[LLL_combined$Treatment=="Rosette Damage/Early Mow",]
-t.test(LLL_RDEM$PreMowLLL, LLL_RDLM$PreMowLLL)
-#This shows that RD+LM plants did not have significantly shorter LLL before mowing than RD+EM plants
-  #One sided t-test is also not significant
 
 #2025####
 #load data
@@ -72,11 +42,14 @@ LLL2025<-read.csv("2025 Pre Mowing LLL Data.csv")
 #Make treatment a factor
 LLL2025$Treatment<-as.factor(LLL2025$Treatment)
 
+#Exclude treatments that include both early and late mow
+LLL2025_filtered <- LLL2025[!(LLL2025$Treatment == "Rosette Damage/Early Mow/Late Mow"|LLL2025$Treatment=="Early Mow/Late Mow"), ]
+
 #run an ANOVA
-anova2025<-aov(lm(Pre.Mow.LLL~Treatment, data=LLL2025))
+anova2025<-aov(lm(Pre.Mow.LLL~Treatment, data=LLL2025_filtered))
 
 #check residuals
-plot(lm(Pre.Mow.LLL~Treatment, data=LLL2025)) #looks fine
+plot(lm(Pre.Mow.LLL~Treatment, data=LLL2025_filtered)) #looks fine
 
 #summarize anova
 summary(anova2025)
@@ -88,7 +61,6 @@ cld(emmeans(anova2025, specs = pairwise~Treatment), Letters=letters)
 
 #RD+LM significantly shorter than LM
 #RD+EM significantly shorter than EM
-#RD+EM+LM is not significantly shorter than EM+LM
 
 #2024####
 #load data
@@ -97,11 +69,14 @@ LLL2024<-read.csv("2024 Pre Mowing LLL Data.csv")
 #Make treatment a factor
 LLL2024$Treatment<-as.factor(LLL2024$Treatment)
 
+#Exclude treatments that include both early and late mow
+LLL2024_filtered <- LLL2024[!(LLL2024$Treatment == "Rosette Damage/Early Mow/Late Mow"|LLL2024$Treatment=="Early Mow/Late Mow"), ]
+
 #run an ANOVA
-anova2024<-aov(lm(PreMowLLL~Treatment, data=LLL2024))
+anova2024<-aov(lm(PreMowLLL~Treatment, data=LLL2024_filtered))
 
 #check residuals
-plot(lm(PreMowLLL~Treatment, data=LLL2024)) #looks fine
+plot(lm(PreMowLLL~Treatment, data=LLL2024_filtered)) #looks fine
 
 #summarize anova
 summary(anova2024)
@@ -113,4 +88,3 @@ cld(emmeans(anova2024, specs = pairwise~Treatment), Letters=letters)
 
 #RD+LM significantly shorter than LM
 #RD+EM is not significantly shorter than EM
-#RD+EM+LM is not significantly shorter than EM+LM
